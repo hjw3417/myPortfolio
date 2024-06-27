b174e4c478461fe1829df9adc12ff3f52f68c8bd
choiyeonhee1 <ccc7598@naver.com>
2024-06-27 11:02:51 +0900

    삭제

--- src/main/java/dc/human/whosthebest/admin/service/AdminGameListService.java
+++
@@ -1,7 +0,0 @@
-package dc.human.whosthebest.admin.service;
-
-import java.util.List;
-
-public interface AdminGameListService {
-    public List listGames() throws Exception;
-}
--- src/main/java/dc/human/whosthebest/admin/service/AdminTeamListService.java
+++
@@ -1,7 +0,0 @@
-package dc.human.whosthebest.admin.service;
-
-import java.util.List;
-
-public interface AdminTeamListService {
-    public List listTeams() throws Exception;
-}
